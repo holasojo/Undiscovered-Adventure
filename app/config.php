86609b6f800834dfc1fc8c106987905d9c566422
Phillip Shin <dsphil7@vt.edu>
2016-04-08 01:50:35 -0400

    Updated config

--- app/config.php
+++ app/config.php
@@ -2,13 +2,13 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://localhost/cs3744/gitHub');
+define('BASE_URL','http://localhost/undiscovered_adventure');
 
 // database constants
 define('DB_HOST','127.0.0.1');
 define('DB_USER','root');
 define('DB_PASS','');
-define('DB_DATABASE','jesus_cs3744db');
+define('DB_DATABASE','cs3744db');
 
 // admin credentials
 define('ADMIN_USERNAME', 'admin');
